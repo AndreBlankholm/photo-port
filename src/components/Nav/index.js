3964b4308e066f89bb51e879daa754d66d2c59e0
AndreBlankholm <ablankholm@gmail.com>
2023-03-14 20:51:37 -0500

    lifting up a level inorder to pass them as props

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
-
   const {
     categories = [],
     setCurrentCategory,
@@ -13,29 +12,26 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a
-              href="#about"
-            >
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li>
+          <li className="mx-2">
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className={`mx-1 ${
+            <li
+              className={`mx-1 ${
                 currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
+                }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
                   setCurrentCategory(category)
