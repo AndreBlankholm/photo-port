a545a3bd7b2ca83732f8e3ea680a5f7a461d172f
AndreBlankholm <ablankholm@gmail.com>
2023-03-15 22:51:51 -0500

    added components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    currentCategory,
-  } = props;
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  useEffect(() => { //this use effect changes the tab title to what your selection is
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
+
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
@@ -28,13 +33,13 @@ function Nav(props) {
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`}
+                currentCategory.name === category.name && "navActive"
+              }`}
               key={category.name}
             >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
